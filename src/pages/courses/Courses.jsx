59807606ce731bde893000869a79395b6a8af6cc
sandeep14032004 <sandeeppati69@gmail.com>
2025-03-17 17:01:39 +0530

    modify main.jsx

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -2,17 +2,31 @@ import React from "react";
 import "./courses.css";
 import { CourseData } from "../../context/CourseContext";
 import CourseCard from "../../components/coursecard/CourseCard";
+import { useNavigate } from "react-router-dom";
+import { UserData } from "../../context/UserContext"; // Importing UserData to get user details
+
 const Courses = () => {
   const { courses } = CourseData();
-  console.log(courses);
+  const { user } = UserData(); // Getting user details
+  const navigate = useNavigate();
+
   return (
     <div className="courses">
       <h2>Available Courses</h2>
+
+      {user?.role === "admin" && (
+        <button
+          className="common-btn"
+          onClick={() => navigate("/admin/course")}
+        >
+          Create Course
+        </button>
+      )}
       <div className="course-container">
         {courses && courses.length > 0 ? (
           courses.map((e) => <CourseCard key={e._id} course={e} />)
         ) : (
-          <p>No Courses.Yet</p>
+          <p>No Courses Yet</p>
         )}
       </div>
     </div>
